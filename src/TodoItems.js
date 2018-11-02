09c45484e3ea9d142e59d5100438fd349ecb53e4
ekayt <ekayusrianto.toisutta@yahoo.com>
2018-11-02 10:49:21 +0800

    Fix #2 TD-0001. Update name from Reminder to To do

--- src/TodoItems.js
+++ src/TodoItems.js
@@ -1,29 +1,35 @@
 import React, { Component } from "react";
- 
+import Typography from '@material-ui/core/Typography'
+
 class TodoItems extends Component {
-	constructor(props) {
+  constructor(props) {
     super(props);
- 
+
     this.createTasks = this.createTasks.bind(this);
   }
   createTasks(item) {
     return <li onClick={() => this.delete(item.key)}
-	key={item.key}>{item.text}</li>
+      key={item.key}>{item.text}</li>
   }
   delete(key) {
     this.props.delete(key);
   }
- 
+
   render() {
     var todoEntries = this.props.entries;
     var listItems = todoEntries.map(this.createTasks);
- 
+
     return (
-      <ul className="theList">
+      <div>
+        <Typography variant='display1' align='center' gutterBottom>
+          Items
+        </Typography>
+        <ul className="theList">
           {listItems}
-      </ul>
+        </ul>
+      </div>
     );
   }
 };
- 
+
 export default TodoItems;
